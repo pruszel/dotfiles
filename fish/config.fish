e4cec7e04d815d491be5f03845b5f2a68d8e5fcd
Peter Ruszel <peter@phpete.me>
2019-07-11 22:52:14 -0700

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
+set -g -x fish_greeting ''
+
 # Add things to PATH
 set -gx PATH $PATH /usr/local/bin/
+set -gx PATH $PATH /Users/peter/Library/Python/2.7/bin/
 
 # Load aliases
 source ~/.config/.aliases
+
+# Node Version Manager
+set -gx NVM_DIR /usr/local/opt/nvm
